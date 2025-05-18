8f397b8395adda66b15c37b76c1a72530ada4f5b
Bozhidar Nemski <bojonemski@gmail.com>
2025-05-18 08:11:02 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ignores: ['cdk.out/*'],
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'max-lines': [
+        'error',
+        {
+          max: 300,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      'import/order': 'error',
+    },
+  },
+  tseslint.configs.recommended,
+]);
