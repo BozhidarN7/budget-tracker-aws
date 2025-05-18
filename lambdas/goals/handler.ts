18665099864c0006463a74401bf9dfcc825b20a5
Bozhidar Nemski <bojonemski@gmail.com>
2025-05-18 09:29:07 +0300

    feat(api): build the budget tracker api, with transactions, categories and goals

---
+++ lambdas/goals/handler.ts
@@ -0,0 +1,70 @@
+import { APIGatewayEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import {
+  DeleteItemCommand,
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  ScanCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+
+const client = new DynamoDBClient({});
+const TABLE_NAME = process.env.TABLE_NAME!;
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+) => {
+  const { httpMethod, pathParameters, body } = event;
+  const id = pathParameters?.id;
+
+  try {
+    if (httpMethod === 'GET' && id) {
+      const res = await client.send(
+        new GetItemCommand({ TableName: TABLE_NAME, Key: marshall({ id }) }),
+      );
+      return { statusCode: 200, body: JSON.stringify(unmarshall(res.Item!)) };
+    }
+
+    if (httpMethod === 'GET') {
+      const res = await client.send(new ScanCommand({ TableName: TABLE_NAME }));
+      return {
+        statusCode: 200,
+        body: JSON.stringify(res.Items?.map((item) => unmarshall(item))),
+      };
+    }
+
+    if (httpMethod === 'POST' && body) {
+      const item = JSON.parse(body);
+      await client.send(
+        new PutItemCommand({ TableName: TABLE_NAME, Item: marshall(item) }),
+      );
+      return { statusCode: 201, body: JSON.stringify({ message: 'Created' }) };
+    }
+
+    if (httpMethod === 'PUT' && id && body) {
+      const updated = JSON.parse(body);
+      updated.id = id;
+      await client.send(
+        new PutItemCommand({ TableName: TABLE_NAME, Item: marshall(updated) }),
+      );
+      return { statusCode: 200, body: JSON.stringify({ message: 'Updated' }) };
+    }
+
+    if (httpMethod === 'DELETE' && id) {
+      await client.send(
+        new DeleteItemCommand({ TableName: TABLE_NAME, Key: marshall({ id }) }),
+      );
+      return { statusCode: 200, body: JSON.stringify({ message: 'Deleted' }) };
+    }
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Unsupported method or missing data.' }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: (err as Error).message }),
+    };
+  }
+};
--- lib/budget-tracker-aws-stack.ts
+++ lib/budget-tracker-aws-stack.ts
@@ -1,16 +1,64 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class BudgetTrackerAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const tables = ['Transaction', 'Category', 'Goal'].reduce(
+      (acc, name) => {
+        acc[name] = new dynamodb.Table(this, `${name}Table`, {
+          partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+          billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+          tableName: name.toLowerCase() + 's',
+          removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+        return acc;
+      },
+      {} as Record<string, dynamodb.Table>,
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BudgetTrackerAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdas = Object.entries(tables).reduce(
+      (acc, [name, table]) => {
+        const handler = new lambda.NodejsFunction(this, `${name}Handler`, {
+          entry: path.join(
+            __dirname,
+            `../lambdas/${name.toLowerCase()}s/handler.ts`,
+          ),
+          handler: 'handler',
+          environment: {
+            TABLE_NAME: table.tableName,
+          },
+        });
+
+        table.grantReadWriteData(handler);
+        acc[name] = handler;
+        return acc;
+      },
+      {} as Record<string, lambda.NodejsFunction>,
+    );
+
+    const api = new apigateway.RestApi(this, 'BudgetTrackerApi', {
+      restApiName: 'Budget Tracker Service',
+      deployOptions: { stageName: 'prod' },
+    });
+
+    Object.entries(lambdas).forEach(([name, lambdaFn]) => {
+      const resource = api.root.addResource(name.toLowerCase() + 's');
+
+      // /<resource> (GET, POST)
+      resource.addMethod('GET', new apigateway.LambdaIntegration(lambdaFn));
+      resource.addMethod('POST', new apigateway.LambdaIntegration(lambdaFn));
+
+      // /<resource>/{id} (GET, PUT, DELETE)
+      const single = resource.addResource('{id}');
+      single.addMethod('GET', new apigateway.LambdaIntegration(lambdaFn));
+      single.addMethod('PUT', new apigateway.LambdaIntegration(lambdaFn));
+      single.addMethod('DELETE', new apigateway.LambdaIntegration(lambdaFn));
+    });
   }
 }
